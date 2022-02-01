ad73c824fe2fa8831a3431ffda5d7fbb70d7f88a
Aluno <guilhermefr4familia@gmail.com>
2022-02-01 17:32:27 -0300

    Integração DataTable

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,4 +1,17 @@
-export const round = (value: number, precision: number) => {
+import { format } from "date-fns";
+
+const round = (value: number, precision: number) => {
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value*multiplier)/multiplier
+}
+
+const formatLocalDate = (date: string, pattern: string) =>{
+    const dt = new Date(date);
+    const dtDateOnly = new Date(dt.valueOf() + dt.getTimezoneOffset()+60+1000);
+    return format(dtDateOnly, pattern)
+}
+
+export {
+    round, 
+    formatLocalDate
 }
